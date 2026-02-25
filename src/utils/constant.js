08d5b7ad7c20e670a18282dbe64b1a9b27977985
Rahul <160149113+Rahul-rathva@users.noreply.github.com>
2026-02-25 15:30:53 +0530

    feat: added a static file

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,3 +3,7 @@ Module.exports ={
     DB_BACKUP_DIR_PREFIX: '_backup_on',
     DB_MANUAL_BACKUP_DIR_PREFIX:'_manual_backup_on',
 };
+
+//stores fixed names 
+//used all caps to tell other deveolopers that this a constant and dont change it during the program
+//if not used this file or faulty, then "variable not defined" error might see
