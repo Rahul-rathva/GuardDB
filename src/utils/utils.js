8a0c622e70205d3e5c116d8ef691f067e2daa5c8
Rahul <160149113+Rahul-rathva@users.noreply.github.com>
2026-02-26 19:25:11 +0530

    feat: added a export Function file for email validation and creating visual table

---
+++ src/utils/utils.js
@@ -0,0 +1,30 @@
+const replaceAll = (str, find, replace) => {
+    return str.replace(new RegExp(find, 'g'), replace);
+};
+
+let validateEmail = (email) => {
+    // A Regular Expression (Regex) to check if an email format is valid
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+};
+
+let createJobsListTable = () => {
+    const Table = require('cli-table3');
+
+    const jobInfoTable = new Table({
+        head: ['JOB', 'JOB STATUS', 'DB TYPE', 'DB NAME', 'BACKUP TIME', 'REMOTE SYNC', 'SMTP'],
+        style: {
+            head: ['bold'],
+            border: [],
+        },
+        colWidths: [16, 14, 12, 16, 16, 14, 12],
+    });
+
+    return jobInfoTable;
+};
+
+module.exports = {
+    replaceAll,
+    validateEmail,
+    createJobsListTable,
+};
